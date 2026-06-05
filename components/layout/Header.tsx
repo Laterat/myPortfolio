7c911fcad276b7bc8eebfb57072790ce5074215f
Latera Mekonnen <mekonnenlatera@gmail.com>
2026-06-05 12:14:20 +0300

    style: made the nav bigger

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="border-b border-surface-border sticky top-0 justify-content">
-      <div className="mx-auto max-w-6xl h-20 px-6 flex items-center justify-between">
+    <header className="border-b border-surface-border sticky top-0">
+      <div className=" h-20 px-6 flex  justify-between">
         <Link href="/" className="flex items-center gap-3">
           <div className="flex h-10 w-10 items-center justify-center rounded-md bg-accent font-bold text-black hover:bg-accent-strong transition-all duration-400  hover:-rotate-15 hover:cursor-pointer">
             L
@@ -15,7 +15,7 @@ export default function Header() {
           </div>
         </Link>
 
-        <nav className="hidden md:flex items-center gap-8 text-sm font-medium">
+        <nav className="hidden md:flex items-center gap-8 sm:text-sm md:text-2xl font-medium">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
           <Link href="/projects">Projects</Link>
@@ -23,7 +23,7 @@ export default function Header() {
         </nav>
 
         <div className="flex items-center gap-3">
-          <button className="rounded-lg border border-surface-border px-4 py-2 text-sm hover:bg-accent transition-colors hover:text-black">
+          <button className=" rounded-lg border border-surface-border px-4 py-2 text-sm hover:bg-accent transition-colors hover:text-black">
             View Work
           </button>
 
